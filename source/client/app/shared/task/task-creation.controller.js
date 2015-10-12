60451c7c2c8f8d59e71e1a580793bd0edfcc83c0
Antoine Dubé <antoine.dube@gmail.com>
2015-10-12 08:36:52 -0400

    WIP: task creation process.

--- source/client/app/shared/task/task-creation.controller.js
+++ source/client/app/shared/task/task-creation.controller.js
@@ -1,113 +1,80 @@
-(function () {'use strict';
+(function () {
+    'use strict';
 
-angular.module('TasksManager.task')
+    angular.module('TasksManager.task')
 
-.controller('NewTaskCtrl', ['$scope', '$modalInstance', function($scope,$modalInstance) {
-    $scope.types = ['Fixe','Assignée','Non-assignée'];
+    .controller('NewTaskCtrl', ['$scope', '$modalInstance', function($scope,$modalInstance) {
+        $scope.types = ['Fixe','Assignée','Non-assignée'];
 
-    $scope.task = {
-        name: '',
-        begin_date: new Date(),
-        end_date: new Date(),
-        deadline_date: new Date(),
-        type: $scope.types[0]
-    };
-    $scope.is_form_complete = false;
+        $scope.task = {
+            name: '',
+            begin_date: new Date(),
+            end_date: new Date(),
+            deadline_date: new Date(),
+            type: $scope.types[0]
+        };
+        $scope.is_form_complete = false;
 
 
-    // Instead of $watch, there is most likely something to do with ngModelController
-    $scope.$watch('task.name', function(newValue,oldValue) {
-        $scope.is_form_complete = (newValue!=='') ? true:false;
-    });
+        // For items specific to the form, create a directive
+        // --> includes model validation
+        //
+        // Instead of $watch, there is most likely something to do with ngModelController
+        $scope.$watch('task.name', function(newValue,oldValue) {
+            $scope.is_form_complete = (newValue!=='') ? true:false;
+        });
 
-    // $scope.form_date_fields.{start_date_required,end_date_required,deadline_required}
-    // TaskUtils.update_required_fields($scope);
-    $scope.is_start_date_required = false;
-    $scope.is_end_date_required = false;
-    $scope.is_deadline_required = false;
-    $scope.$watch('task.type', function(newValue,oldValue) {
-        if (newValue===$scope.types[0]) {
-            $scope.is_start_date_required = true;
-            $scope.is_end_date_required = true;
-            $scope.is_deadline_required = false;
-        }
-        else if (newValue===$scope.types[1]) {
-            $scope.is_start_date_required = true;
-            $scope.is_end_date_required = true;
-            $scope.is_deadline_required = true;
-        }
-        else if (newValue===$scope.types[2]) {
-            $scope.is_start_date_required = false;
-            $scope.is_end_date_required = false;
-            $scope.is_deadline_required = true;
-        }
-    });
+        $scope.is_start_date_required = false;
+        $scope.is_end_date_required = false;
+        $scope.is_deadline_required = false;
+        $scope.$watch('task.type', function(newValue,oldValue) {
+            if (newValue===$scope.types[0]) {
+                $scope.is_start_date_required = true;
+                $scope.is_end_date_required = true;
+                $scope.is_deadline_required = false;
+            }
+            else if (newValue===$scope.types[1]) {
+                $scope.is_start_date_required = true;
+                $scope.is_end_date_required = true;
+                $scope.is_deadline_required = true;
+            }
+            else if (newValue===$scope.types[2]) {
+                $scope.is_start_date_required = false;
+                $scope.is_end_date_required = false;
+                $scope.is_deadline_required = true;
+            }
+        });
 
-    $scope.create = function() {
-        var new_task = {};
-        new_task.name = $scope.task.name;
-        new_task.type = $scope.task.type;
-        // Create some function/module/factory to transform $scope.task to a Task object new_task of correct type (fixed, assigned, ...)
-        // new_task = TaskHelpers.create($scope.task)
-        if (new_task.type===$scope.types[0]) {
-            new_task.begin_date = $scope.task.begin_date;
-            new_task.begin_date.setHours($scope.task.begin_time.getHours(),$scope.task.begin_time.getMinutes());
+        $scope.open_begin_date = function($event) {
+            $event.preventDefault();
+            $event.stopPropagation();
 
-            new_task.end_date = $scope.task.end_date;
-            new_task.end_date.setHours($scope.task.end_time.getHours(),$scope.task.end_time.getMinutes());
+            $scope.begin_date_opened = true;
+        };
 
-            new_task.deadline = null;
-        }
-        else if (new_task.type===$scope.types[1]) {
-            new_task.begin_date = $scope.task.begin_date;
-            new_task.begin_date.setHours($scope.task.begin_time.getHours(),$scope.task.begin_time.getMinutes());
+        $scope.open_end_date = function($event) {
+            $event.preventDefault();
+            $event.stopPropagation();
 
-            new_task.end_date = $scope.task.end_date;
-            new_task.end_date.setHours($scope.task.end_time.getHours(),$scope.task.end_time.getMinutes());
+            $scope.end_date_opened = true;
+        };
 
-            new_task.deadline = $scope.task.deadline_date;
-            new_task.deadline.setHours($scope.task.deadline_time.getHours(),$scope.task.deadline_time.getMinutes());
-        }
-        else if (new_task.type===$scope.types[2] && typeof($scope.task.deadline_time)!='undefined') {
-            new_task.begin_date = null;
-            new_task.end_date = null;
-            new_task.deadline = $scope.task.deadline_date;
-            new_task.deadline.setHours($scope.task.deadline_time.getHours(),$scope.task.deadline_time.getMinutes());
-        }
-        else {
-            new_task.begin_date = null;
-            new_task.end_date = null;
-            new_task.deadline = null;
-        }
+        $scope.open_deadline_date = function($event) {
+            $event.preventDefault();
+            $event.stopPropagation();
 
-        console.log('on modal close: ', new_task);
+            $scope.deadline_date_opened = true;
+        };
+        // Until here should be included in modal
 
-        $modalInstance.close(new_task);
-    };
 
-    $scope.cancel = function() {
-        $modalInstance.dismiss();
-    };
+        $scope.create = function() {
+            $modalInstance.close($scope.task);
+        };
 
-    $scope.open_begin_date = function($event) {
-        $event.preventDefault();
-        $event.stopPropagation();
+        $scope.cancel = function() {
+            $modalInstance.dismiss();
+        };
 
-        $scope.begin_date_opened = true;
-    };
-
-    $scope.open_end_date = function($event) {
-        $event.preventDefault();
-        $event.stopPropagation();
-
-        $scope.end_date_opened = true;
-    };
-
-    $scope.open_deadline_date = function($event) {
-        $event.preventDefault();
-        $event.stopPropagation();
-
-        $scope.deadline_date_opened = true;
-    };
-}]);
+    }]);
 })();
--- source/client/app/shared/task/task.factory.js
+++ source/client/app/shared/task/task.factory.js
@@ -26,15 +26,49 @@
                 windowClass: 'dropdown-menu-right'
             }).result.then(function(task) {
                 console.log('in TaskFactory.create(): ', task);
-                return new Task({
-                    'name': task.name,
-                    'begin_at': task.begin_date,
-                    'end_at': task.end_date,
-                    'deadline': null,
-                    'duration': task.end_date.getTime() - task.begin_date.getTime(),
-                    'type': task.type
-                }).$save();
+                var new_task = create_from_type(task);
+                return new_task.$save();
             });
+        }
+
+        function create_from_type(task) {
+            var new_task = {};
+            switch (task.type) {
+                case 'Fixe':
+                    new_task = new Task({
+                        'name': task.name,
+                        'begin_at': task.begin_date,
+                        'end_at': task.end_date,
+                        'type': 'Fixe'
+                    })
+                    break;
+                case 'Assignée':
+                    new_task = new Task({
+                        'name': task.name,
+                        'begin_at': task.begin_date,
+                        'end_at': task.end_date,
+                        'deadline': task.deadline,
+                        'type': 'Assignée'
+                    })
+                    break;
+                case 'Non-assignée':
+                    if (typeof task.deadline !== 'undefined') {
+                        new_task = new Task({
+                            'name': task.name,
+                            'deadline': task.deadline,
+                            'type': 'Non-assignée'
+                        })
+                    } else {
+                        new_task = new Task({
+                            'name': task.name,
+                            'type': 'Non-assignée'
+                        })
+                    }
+                    break;
+                default:
+                    console.log('Undefined type: ', task.type);
+            }
+            return new_task;
         }
     }
 })();
