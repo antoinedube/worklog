88543e3182283555affa0a33713f120515c5700a
Antoine Dubé <antoine.dube@gmail.com>
2015-10-15 21:27:34 -0400

    Fixed karma files include patterns.

--- source/client/karma.conf.js
+++ source/client/karma.conf.js
@@ -4,14 +4,18 @@ module.exports = function(config){
     basePath : './',
 
     files : [
-      'app/bower_components/angular/angular.js',
-      'app/bower_components/angular-resource/angular-resource.js',
-      'app/bower_components/angular-route/angular-route.js',
-      'app/bower_components/angular-mocks/angular-mocks.js',
-      'app/bower_components/angular-bootstrap/ui-bootstrap-tpls.js',
-      'app/components/**/*.js',
-      'app/shared/**/*.js',
-      'app/shared/**/**/*.js',
+      'public/bower_components/angular/angular.js',
+      'public/bower_components/angular-activerecord/src/angular-activerecord.js',
+      'public/bower_components/angular-resource/angular-resource.js',
+      'public/bower_components/angular-route/angular-route.js',
+      'public/bower_components/angular-mocks/angular-mocks.js',
+      'public/bower_components/angular-bootstrap/ui-bootstrap-tpls.js',
+      'app/**/**/*.model.js',
+      'app/**/**/*.factory.js',
+      'app/**/**/*.config.js',
+      'app/**/**/*.controller.js',
+      'app/**/**/*.directive.js',
+      'app/**/**/*.test.js'
     ],
 
     autoWatch : true,
