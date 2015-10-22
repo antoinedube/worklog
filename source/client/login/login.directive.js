a3470a331b40d86364a42b903879b8986d3058df
Antoine Dubé <antoine.dube@gmail.com>
2015-10-22 13:10:46 -0400

    Changed indentation.

--- source/client/login/login.directive.js
+++ source/client/login/login.directive.js
@@ -1,55 +1,54 @@
 (function () {
-    'use strict';
+  'use strict';
 
-    angular
-        .module('TasksManagerLogin')
-        .directive('frontPageLogin', FrontPageLogin);
+  angular
+    .module('TasksManagerLogin')
+    .directive('frontPageLogin', FrontPageLogin);
 
-    function FrontPageLogin() {
-        var directive =  {
-            controller: FrontPageLoginController,
-            bindToController: true,
-            controllerAs: 'vm',
-            link: FrontPageLoginLink,
-            replace: true,
-            restrict: 'EA',
-            scope: {},
-            templateUrl: 'task_manager_login/login.view.html'
-        };
+  function FrontPageLogin() {
+    var directive = {
+      controller: FrontPageLoginController,
+      bindToController: true,
+      controllerAs: 'vm',
+      link: FrontPageLoginLink,
+      replace: true,
+      restrict: 'EA',
+      scope: {},
+      templateUrl: 'task_manager_login/login.view.html'
+    };
 
-        return directive;
+    return directive;
+  }
+
+  FrontPageLoginController.$inject = ['$resource', '$window'];
+  function FrontPageLoginController($resource, $window) {
+    var vm = this;
+    vm.user = {};
+    vm.login_message = '';
+    vm.login_message_class = '';
+
+    vm.submit = submit;
+
+    /* ---------- */
+
+    function submit() {
+      console.log('User: ', vm.user);
+      $resource('/login',{})
+        .save(vm.user).$promise
+        .then(function(data) {
+          vm.user = {};
+          vm.login_message = 'Connexion réussie';
+          vm.login_message_class = 'success';
+          $window.location.reload();
+        }, function(data) {
+          vm.user = {};
+          vm.login_message = 'Connexion refusée';
+          vm.login_message_class = 'error';
+        });
     }
+  }
 
-    FrontPageLoginController.$inject = ['$resource', '$window'];
-    function FrontPageLoginController($resource, $window) {
-        var vm = this;
-        vm.user = {};
-        vm.login_message = '';
-        vm.login_message_class = '';
-
-        vm.submit = submit;
-
-        /* ---------- */
-
-        function submit() {
-            console.log('User: ', vm.user);
-            $resource('/login',{})
-                .save(vm.user).$promise
-                .then(function(data) {
-                    console.log('Success: ', data);
-                    vm.login_message = 'Connexion réussie';
-                    vm.login_message_class = 'success';
-                    $window.location.reload();
-                }, function(data) {
-                    console.log('Error status: ', data.status);
-                    vm.login_message = 'Connexion refusée';
-                    vm.login_message_class = 'error';
-                    console.log('login message: ', vm.login_message);
-                });
-        }
-    }
-
-    function FrontPageLoginLink(scope, element, attrs, vm) {
-    }
+  function FrontPageLoginLink(scope, element, attrs, vm) {
+  }
 
 })();
