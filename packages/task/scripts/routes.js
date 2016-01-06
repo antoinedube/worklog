b722a5909123ac29c38f9e47a00d012016c6665f
Antoine Dubé <antoine.dube@gmail.com>
2016-01-06 16:22:34 -0500

    Changed date format in task routes.

--- packages/task/scripts/routes.js
+++ packages/task/scripts/routes.js
@@ -1,7 +1,7 @@
 Router.route('/tasks_list/:date', {
   template: 'tasks_list',
   data: function() {
-    date = moment(this.params.date);
+    date = moment(this.params.date).format('YYYY-MM-DD');
     return {
       date: date
     }
