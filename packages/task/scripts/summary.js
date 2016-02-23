e7b12a494ed6d176678bed06c3df2966c1ba4ff8
Antoine Dubé <antoine.dube@gmail.com>
2016-02-23 13:31:41 -0500

    Added workday summary.

--- packages/task/scripts/summary.js
+++ packages/task/scripts/summary.js
@@ -1,23 +1,33 @@
 if (Meteor.isClient) {
+
+  function criteria_today(date) {
+    return {
+      "$gte": moment(date).startOf('day').toDate(),
+      "$lt": moment(date).add(1, 'days').startOf('day').toDate()
+    };
+  }
+
+  function sum_duration(tasks) {
+    var sum_duration = 0;
+    _.forEach(tasks, function(task) {
+      sum_duration += task.duration;
+    });
+    return sum_duration;
+  }
+
   Template.summary.helpers({
-    workday_summary: function() {
-      var todays_tasks = Tasks.find(
-        {
-          begin_time: {
-            "$gte": moment(this.date).startOf('day').toDate(),
-            "$lt": moment(this.date).add(1, 'days').startOf('day').toDate()
-          }
-        },
-        {
-          sort: {
-            begin_time: +1
-          }
-        }
-      );
-      todays_tasks.forEach(function(task) {
-        console.log('Task: ', task);
-      });
-      // return array of type/time
+    analysis_count: function() {
+      var search_criteria = { begin_time: criteria_today(this.date), type: "100 - Analysis" };
+      var tasks = Tasks.find(search_criteria).fetch();
+
+      return sum_duration(tasks);
+    },
+
+    planning_count: function() {
+      var search_criteria = { begin_time: criteria_today(this.date), type: "104 - Planning/Scrum" };
+      var tasks = Tasks.find(search_criteria).fetch();
+
+      return sum_duration(tasks);
     }
   });
 }
